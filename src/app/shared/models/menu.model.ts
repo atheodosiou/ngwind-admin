3b8200986167b8c515a50d6cb1669e5dd544c551
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2021-12-11 18:18:51 +0200

    Add menuItem model apply dark mode on sidebar and navbar

---
+++ src/app/shared/models/menu.model.ts
@@ -0,0 +1,15 @@
+export interface MenuItem {
+    id?: string;
+    label?: string;
+    icon?: string;
+    command?: () => void;
+    url?: string;
+    routerLink?: string[];
+    state?: any;
+    items?: MenuItem[];
+    expanded?: boolean;
+    disabled?: boolean;
+    visible?: boolean;
+    style?: any;
+    styleClass?: string;
+}

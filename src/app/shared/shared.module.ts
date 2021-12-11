2f81fd5fb0e13dc8a273dfff4a47149b81cda2e1
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2021-12-11 18:42:47 +0200

    Add footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FooterComponent } from "./components/footer/footer.component";
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { SideBarComponent } from "./components/side-bar/side-bar.component";
 import { SideBarService } from "./services/side-bar.service";
 
 const components = [
     NavBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    FooterComponent
 ];
 
 @NgModule({
